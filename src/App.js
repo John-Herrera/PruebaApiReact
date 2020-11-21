3e91d4485ba05dce6144487043e84ccfcad5f765
John-Herrera <jferney69@hotmail.com>
2020-11-21 16:00:23 -0500

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app container">
+      <div className="jumbotron" >
+        <p className="lead text-center">Bucador</p>
+        
+      </div>            
+    </div>
+  );
+}
+
+export default App;
